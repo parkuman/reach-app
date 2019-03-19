12d374e62b62f704d7353e26c5d4741242eebaf1
Parkuman <parkumania@gmail.com>
2019-03-19 14:10:55 -0400

    added google maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyD1VdxIGDCfnFyPRJVzRHO0v3e1TK_fTtk"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../scoped_models/main_model.dart';
 
@@ -14,21 +17,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // the controller that controls the google map
+  Completer<GoogleMapController> _mapController = Completer();
+
+  static const LatLng _initialCameraPosition =
+      const LatLng(45.521563, -122.677433);
+
   @override
   void initState() {
     super.initState();
   }
 
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Signed In as:'),
-          Text(widget.model.authenticatedUser.email),
-        ],
-      ),
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _initialCameraPosition,
+            zoom: 14,
+          ),
+        ),
+        
+      ],
     );
   }
 }
