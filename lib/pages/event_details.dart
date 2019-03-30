7c75fc3bd6552d39a0d460208db9223b64e5f246
Parkuman <parkumania@gmail.com>
2019-03-30 14:41:19 -0400

    reach/unreach

--- lib/pages/event_details.dart
+++ lib/pages/event_details.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
 import '../scoped_models/main_model.dart';
 
-class EventDetailsPage extends StatelessWidget {
+class EventDetailsPage extends StatefulWidget {
   final MainModel model;
   final int eventIndex;
   EventDetailsPage(this.model, {this.eventIndex});
 
   @override
+  _EventDetailsPageState createState() => _EventDetailsPageState();
+}
+
+class _EventDetailsPageState extends State<EventDetailsPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${eventIndex + 1}'),
+        title: Text('${widget.eventIndex + 1}'),
       ),
-      body: Center(
-        child: RaisedButton(
-          color: Theme.of(context).accentColor,
-          child: Text('REACH'),
-          onPressed: () {
-            model.reachEvent(model.displayedEvents[eventIndex]);
-          },
-        ),
-      ),
+      body: Center(child: ScopedModelDescendant(
+        builder: (BuildContext context, Widget child, MainModel model) {
+          return RaisedButton(
+            color: Theme.of(context).accentColor,
+            // if loading display a progrss indicator
+            child: model.isLoading
+                ? CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  )
+                // if not display text, display reach if the user isnt going yet, display UNREACH if the user is
+                : Text(model.allEvents[widget.eventIndex].attendees
+                        .contains(model.authenticatedUser.id)
+                    ? 'UN-REACH'
+                    : 'REACH'),
+            onPressed: () {
+              widget.model.reachEvent(model.displayedEvents[widget.eventIndex]);
+            },
+          );
+        },
+      )),
     );
   }
 }
